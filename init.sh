5ad923735e0a0acc75a5f571604e70b7f67f8a52
Matthias Kesenheimer <matthias.kesenheimer@syss.de>
2021-10-04 08:54:07 +0200

    modified init script

--- init.sh
+++ init.sh
@@ -3,9 +3,14 @@
 git init
 git submodule add https://github.com/xscorp/Burpee.git
 git branch -M master
+
+# remote: all
 git remote add all git@github.com:MKesenheimer/leldap.git
 git remote set-url --add --push all git@github.com:MKesenheimer/leldap.git
 git remote set-url --add --push all git@ptmk.sy.gs:remotes/leldap.git
+
+# remote: origin
+git remote add origin git@ptmk.sy.gs:remotes/leldap.git
 git remote set-url --add --push origin git@ptmk.sy.gs:remotes/leldap.git
 
 # add and push to root
